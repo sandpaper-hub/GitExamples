544c2bb7cda01f7b1a7590c0431ca504a9224f4b
Constantine Kim <electro.passion92@gmail.com>
2020-04-26 00:57:41 +0900

    Test created

--- src/com/crazyEx/Main.java
+++ src/com/crazyEx/Main.java
@@ -1,7 +1,11 @@
 package com.crazyEx;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<Member<String>> crazyMembersList = new ArrayList<>();
+        crazyMembersList.add(new Member<>("Anik", 30, false));
+        crazyMembersList.add(new Member<>("Nika", 27, true));
     }
 }
